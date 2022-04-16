03a02b88e152e0d937769c1f7d8e71286a98c2d1
loilm <lemauloi0395@gmail.com>
2022-04-16 16:18:53 +0700

    Fix alert with wait

--- src/test/java/pages/JavaScriptPage.java
+++ src/test/java/pages/JavaScriptPage.java
@@ -1,13 +1,21 @@
 package pages;
 
 import commons.CommonPage;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+
+import java.time.Duration;
 
 public class JavaScriptPage {
     private WebDriver driver;
     private CommonPage commonPage;
+    private WebDriverWait wait;
+    private Alert alert;
+
     By clickForJSAlertBtn = By.xpath("//button[.='Click for JS Alert']");
     By clickForJSConfirmBtn = By.xpath("//button[.='Click for JS Confirm']");
     By clickForJSPromptBtn = By.xpath("//button[.='Click for JS Prompt']");
@@ -16,29 +24,33 @@ public class JavaScriptPage {
     public JavaScriptPage(WebDriver driver) {
         this.driver = driver;
         commonPage = new CommonPage(driver);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     public void clickForJSAlert() {
         commonPage.clickElement(clickForJSAlertBtn);
-        driver.switchTo().alert().accept();
+        alert = wait.until(ExpectedConditions.alertIsPresent());
+        alert.accept();
     }
 
     public void clickForJSConfirm(String action) {
         commonPage.clickElement(clickForJSConfirmBtn);
+        alert = wait.until(ExpectedConditions.alertIsPresent());
         if (action.trim().equalsIgnoreCase("accept")) {
-            driver.switchTo().alert().accept();
+            alert.accept();
         } else if (action.trim().equalsIgnoreCase("dismiss")) {
-            driver.switchTo().alert().dismiss();
+            alert.dismiss();
         }
     }
 
     public void clickForJSPrompt(String enterText, String action) {
         commonPage.clickElement(clickForJSPromptBtn);
-        driver.switchTo().alert().sendKeys(enterText);
+        alert = wait.until(ExpectedConditions.alertIsPresent());
+        alert.sendKeys(enterText);
         if (action.trim().equalsIgnoreCase("accept")) {
-            driver.switchTo().alert().accept();
+            alert.accept();
         } else if (action.trim().equalsIgnoreCase("dismiss")) {
-            driver.switchTo().alert().dismiss();
+            alert.dismiss();
         }
     }
 
