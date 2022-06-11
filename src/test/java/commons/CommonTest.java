ba9c61820ebeb1b7f6b81f792ee381ff8f93dc9d
loilm <lemauloi0395@gmail.com>
2022-06-11 10:55:17 +0700

    Commit func: capture screen and record video

--- src/test/java/commons/CommonTest.java
+++ src/test/java/commons/CommonTest.java
@@ -1,11 +1,27 @@
 package commons;
 
+import atu.testrecorder.ATUTestRecorder;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class CommonTest {
-//    Setting browser and run testcase with .xml file
+    private ATUTestRecorder recorder;
+    private DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy_HH-mm-ss");
+    private Date date = new Date();
+
+    //    Setting browser and run testcase with .xml file
     @Parameters("browser")
     @BeforeClass
     public void baseSetup(String browserName) {
@@ -16,6 +32,35 @@ public class CommonTest {
 //    public void baseSetup() {
 //        Browser.browserSetup("chrome");
 //    }
+
+    @AfterMethod
+    public void captureScreen(ITestResult testResult) throws IOException {
+        if (!testResult.isSuccess()) {
+            TakesScreenshot takesScreenshot = (TakesScreenshot) Browser.getDriver();
+            File filePath = new File("./screenshot/");
+            if (!filePath.exists()) {
+                filePath.mkdirs();
+            }
+            File srcFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
+            File destFile = new File(filePath + testResult.getName() + "_" + dateFormat.format(date) + ".png");
+            FileUtils.copyFile(srcFile, destFile);
+        }
+    }
+
+//    ATUTestRecorder library
+    public void startRecordVideo(String videoName) throws Exception {
+        File filePath = new File("./videos/");
+        if (!filePath.exists()) {
+            filePath.mkdirs();
+        }
+        recorder = new ATUTestRecorder(String.valueOf(filePath), videoName + "_" + dateFormat.format(date), false);
+        recorder.start();
+    }
+
+//    ATUTestRecorder library
+    public void stopRecordVideo() throws Exception {
+        recorder.stop();
+    }
 
     @AfterClass
     public void tearDown() {
