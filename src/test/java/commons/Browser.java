bdc7c80a26e25ad92b20c8da9c543ac3fa176afb
loilm <lemauloi0395@gmail.com>
2022-07-08 09:35:56 +0700

    add headless mode

--- src/test/java/commons/Browser.java
+++ src/test/java/commons/Browser.java
@@ -3,8 +3,11 @@ package commons;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class Browser {
     private static WebDriver driver;
@@ -23,10 +26,29 @@ public class Browser {
                 WebDriverManager.edgedriver().setup();
                 driver = new EdgeDriver();
                 break;
+            case "chrome-headless":
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.setHeadless(true);
+                driver = new ChromeDriver(chromeOptions);
+                break;
+            case "firefox-headless":
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.setHeadless(true);
+                driver = new FirefoxDriver(firefoxOptions);
+                break;
+            case "edge-headless":
+                WebDriverManager.edgedriver().setup();
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.setHeadless(true);
+                driver = new EdgeDriver(edgeOptions);
+                break;
             default:
                 System.out.println("Browser '" + browserName + "' not supported.");
         }
         driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
         return driver;
     }
 
